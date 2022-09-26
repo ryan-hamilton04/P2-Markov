c6f737b80aad4bcfc6ff1f9c0982ee1c09f5321b
Brandon Fain <brandon.fain@duke.edu>
2022-09-26 13:09:38 +0000

    set PRINT_MODE to true by default

--- src/MarkovDriver.java
+++ src/MarkovDriver.java
@@ -15,7 +15,7 @@ public class MarkovDriver {
 	private static int TEXT_SIZE = 100;
 	private static long RANDOM_SEED = 1234;
 	private static int MODEL_ORDER = 2;
-	private static boolean PRINT_MODE = false;
+	private static boolean PRINT_MODE = true;
 	
 	public static void main(String[] args) {
 		// Can change the training text here
